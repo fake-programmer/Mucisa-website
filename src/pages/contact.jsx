4b7b537e705b717101dfcf8fe3cf6f6cc77ed423
fake-programmer <ndegeezra05@gmail.com>
2025-06-03 13:01:47 +0300

    added contact functionality

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,17 +1,35 @@
   import React from 'react';
    import '../CSS-styling/contact.css';
-
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
      const Contact = () => {
-       return (
-         <div className= "container">
-           <form className= "form">
-             <input type="text" placeholder="Name" className="input" />
- <input type="email" placeholder="Email" className="input" />
-             <textarea placeholder="Message" className="input"></textarea>
-             <button type="submit" >Send</button>
-           </form>
-         </div>
-       );
-     };
+       const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ex2j4bm', 'template_p7ovwsa', form.current, '_wm3KswFnZELfE6pw')
+      .then((result) => {
+          console.log(result.text);
+          alert('Message sent successfully!');
+      }, (error) => {
+          console.log(error.text);
+          alert('Failed to sent.');
+      });
+
+    e.target.reset();
+  };
+
+  return (
+    <div className="container">
+    <form ref={form} onSubmit={sendEmail} className="form">
+      <input className="input" type="text" name="name" placeholder="Your Name" required />
+ <input  className="input" type="email" name="email" placeholder="Your Email" required />
+      <textarea className="input"  name="message" placeholder="Your Message" required />
+      <button type="submit">Send</button>
+    </form>
+    </div>
+  );
+}
 
      export default Contact;
